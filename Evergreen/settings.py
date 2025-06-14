31c4f943f3c53476abceffa0f9407e0b29a6b541
Shoaib Anwar <124421743+shoaibanwar13@users.noreply.github.com>
2025-06-14 22:01:48 +0500

    static file apply

--- Evergreen/settings.py
+++ Evergreen/settings.py
@@ -163,12 +163,15 @@ MESSAGE_TAGS={
     messages.ERROR:'danger'
 }
 import os
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static")
-    
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # this must match Nginx
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT=os.path.join(BASE_DIR,'assests')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 MEDIA_URL ='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,"media")
 #SMTP 
