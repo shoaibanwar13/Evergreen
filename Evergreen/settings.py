f8148dd1d64c96aa08fcd02d4ce11980ad29bb0d
Shoaib Anwar <124421743+shoaibanwar13@users.noreply.github.com>
2025-06-18 17:21:45 +0500

    Update settings.py

--- Evergreen/settings.py
+++ Evergreen/settings.py
@@ -201,8 +201,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'evergreencornsilage@gmail.com'
-EMAIL_HOST_PASSWORD = 'pgud imzt zbus sukm'
+EMAIL_HOST_USER = 'shoaib0033237@gmail.com'
+EMAIL_HOST_PASSWORD = 'knkn mdph pydx fqzq'
 
 
 
