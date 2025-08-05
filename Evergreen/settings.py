bd5fc57b2ce373886007834b8243d893a077e852
shoaib Anwer <shoaib0033237@gmail.com>
2025-08-05 23:27:39 +0500

    middle

--- Evergreen/settings.py
+++ Evergreen/settings.py
@@ -94,6 +94,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_htmx.middleware.HtmxMiddleware',
+    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'Evergreen.urls'
